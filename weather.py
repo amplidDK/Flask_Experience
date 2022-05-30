5b488eecd96e7eacbc9abbd2bb96556245161b26
amplidDK <gringo13>
2022-05-30 22:44:16 +0300

    First initial commit. You may run server and get an info. But it still separated.

---
+++ weather.py
@@ -0,0 +1,28 @@
+import requests
+
+import setting
+
+
+def weather_by_city(city, period):
+    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    payload = {
+        'key': setting.API_KEY,
+        'q': city,
+        'format': 'json',
+        'num_of_days': period,
+        'lang': 'ru'
+    }
+
+    response = requests.get(weather_url, params=payload)
+    result = response.json()
+    if 'data' in result:
+        if 'current_condition' in result['data']:
+            try:
+                return result['data']['current_condition'][0]
+            except(IndexError, TypeError):
+                return False
+    return False
+
+
+if __name__ == '__main__':
+    print(weather_by_city('Moscow', 3))
